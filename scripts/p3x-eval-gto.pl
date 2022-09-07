bbd60c93a67fdace9a312a7ed77ecf9926d27b55
Bruce Parrello <bparrello@figresearch.com>
2022-09-07 15:10:38 -0500

    Converted to use dl4j.eval for evaluations.

--- scripts/p3x-eval-gto.pl
+++ scripts/p3x-eval-gto.pl
@@ -6,8 +6,8 @@ This script evaluates a single L<GenomeTypeObject> and stores the quality data b
 
 =head2 Parameters
 
-The positional parameters are the file name of the L<GenomeTypeObject>, the name of the output file for the evaluation results, and the name of a web page file
-for the quality analysis.
+The positional parameters are the file name of the input L<GenomeTypeObject>, the name of an output file for status information,
+and the name of a web page file for the quality analysis.
 
 Additional command-line options are as follows:
 
@@ -26,70 +26,41 @@ If this option is specified and C<ref> is not specified, a reference genome will
 
 The name of the directory containing the evaluation files. The default is C<Eval> in the global data directory.
 
-=item template
-
-The name of the template file. The default is C<p3_code/lib/BinningReports/webdetails.tt> in the SEEDtk module directory.
-
-=item external
-
-If specifed, the incoming genome is presumed to be external, and no contig links will be generated on the web page.
-
-=item binned
-
-If specified, the incoming genome is presumed to have external contig IDs which are stored in the description fields of
-the sequences in PATRIC.
-
-=item parallel
-
-The number of parallel processes to run when applying the function predictors. The default is C<8>.
-
-=item improve
-
-If specified, the name of a FASTA file.  A reduced set of contigs representing an improved genome will be written to
-this file, if an improvement is possible.
-
 =item workDir
 
-Name of a working directory for creating the matrix.  If none is specified, a temporary directory will be used.
+Name of a working directory for intermediate files.  If none is specified, a temporary directory will be used.
 
-=item checkDir
+=item test
 
-The name of the directory containing the reference genome table and the completeness data files. The default
-is to use the directory computed by C<--evalDir>.
-
-=item predictors
-
-The name of the directory containing the role definition files and the function predictors for the consistency
-checking. The default is to use the directory computed by C<--evalDir>.
-
+If specified, the new GTO will be left in the work directory with the name "eval.gto", and the original GTO will
+not be updated.
 
 =back
 
 =cut
 
 use strict;
-use P3DataAPI;
 use P3Utils;
-use EvalHelper;
-use GenomeTypeObject;
+use P3DataAPI;
+use File::Temp;
+use File::Copy::Recursive;
 
 # Get the command-line options.
 my $opt = P3Utils::script_opts('gtoFile outFile outHtml',
         ['ref|r=s', 'reference genome ID (implies deep)'],
         ['deep', 'if specified, the genome is compared to a reference genome for more detailed analysis'],
         ['evalDir|eval=s', 'evaluation data directory', { default => "$FIG_Config::p3data/Eval" }],
-        ['checkDir=s', 'completeness data directory'],
-        ['predictors=s', 'function predictors directory'],
-        ['template=s', 'template for web pages', { default => "$FIG_Config::mod_base/p3_code/lib/BinningReports/webdetails.tt" }],
-        ['external', 'the genome is not currently installed in PATRIC'],
-        ['binned', 'the genome contig IDs are user-suppled, not PATRIC-generated'],
-        ['improve=s', 'name of a FASTA file to contain an improved version of the GTO contigs'],
-        ['parallel=i', 'parallelism to use in matrix evaluation', { default => 8 }],
-        ['workDir=s', 'name of a working directory for the evaluation matrix'],
-        ['genomeBaseUrl=s', 'Base path for PATRIC/BVBRC genome page'],
+        ['checkDir=s', 'no longer used'],
+        ['predictors=s', 'no longer used'],
+        ['template=s', 'no longer used'],
+        ['external', 'no longer used'],
+        ['binned', 'no longer used'],
+        ['improve=s', 'no longer used'],
+        ['parallel=i', 'no longer used'],
+        ['workDir=s', 'name of a working directory for the java output (erased after use)'],
+        ['genomeBaseUrl=s', 'no longer used'],
+        ['test', 'test evaluation without updated GTO']
         );
-# Get access to PATRIC.
-my $p3 = P3DataAPI->new();
 # Get the input parameters.
 my ($gtoFile, $outFile, $outHtml) = @ARGV;
 if (! $gtoFile) {
@@ -99,20 +70,58 @@ if (! $gtoFile) {
 } elsif (($opt->ref || $opt->deep) && ! $outHtml) {
     die "No output web page file specified.";
 }
-# Read in the GTO.
-my $gto = GenomeTypeObject->create_from_file($gtoFile);
-if (! $gto) {
-    die "Invalid or missing gto file $gtoFile.";
+# Verify the GTO.
+if (! -s $gtoFile) {
+    die "Input GTO file is empty or not found.";
 }
-# Call the main processor.
-my $checkDir = $opt->checkdir // ($opt->evaldir . "/CheckR");
-my $funcDir = $opt->predictors // ($opt->evaldir . "/FunctionPredictors");
-my $geo = EvalHelper::ProcessGto($gto, 'ref' => $opt->ref, deep => $opt->deep, checkDir => $checkDir, predictors => $funcDir,
-				 parallel => $opt->parallel, workDir => $opt->workdir,
-				 p3 => $p3, outFile => $outFile, outHtml => $outHtml, template => $opt->template, external => $opt->external, binned => $opt->binned,
-				 improve => $opt->improve,
-				 ($opt->genomebaseurl ? (outputGenomeBaseURL => $opt->genomebaseurl) : ()),
-				);
-# Write the results.
-$gto->destroy_to_file($gtoFile);
+# Find the work directory.
+my $workDir = $opt->workdir;
+my $tmpObject;
+if (! $workDir) {
+    $tmpObject = File::Temp->newdir;
+    $workDir = $tmpObject->dirname;
+} elsif (! -d $workDir) {
+    File::Copy::Recursive::pathmk($workDir) || die "Could not create work directory: $!";
+}
+my $jarDir = $ENV{SEED_JARS};
+if (! $jarDir) {
+    die "SEED_JARS environment variable must be specified!";
+}
+if (! $ENV{P3API_URL}) {
+    # We need the P3 data API URL in the environment.
+    my $p3 = P3DataAPI->new();
+    $ENV{P3API_URL} = $p3->{url};
+}
+# Format the parameters.
+my $bufferFile = "$workDir/eval.gto";
+my $reportFile = "$workDir/GenomeReport.html";
+my @parms = ("gto", "--home", "BV-BRC", "--outDir", $workDir, "--output", $bufferFile, "--input", $gtoFile);
+if ($opt->ref) {
+    push @parms, "--ref", $opt->ref;
+}
+push @parms, "--format";
+if ($opt->deep) {
+    push @parms, "DEEP";
+} else {
+    push @parms, "HTML";
+}
+if ($opt->test) {
+    push @parms, "--verbose";
+}
+push @parms, $opt->evaldir;
+# Clear the log file.
+if (-s $outFile) {
+    unlink $outFile;
+}
+# Call the evaluator.
+my $rc = system(java => "-Dlogback.configurationFile=$jarDir/dl4j.eval.logback.xml", "-Dlogfile.name=$outFile", "-jar",
+        "$jarDir/dl4j.eval.jar", @parms);
+if ($rc) {
+    die "Error in evaluation: rc = $rc.";
+}
+# Copy results from work directory.
+File::Copy::Recursive::fcopy($reportFile, $outHtml) || die "Could not copy html file: $!";
+if (! $opt->test) {
+    File::Copy::Recursive::fcopy($bufferFile, $gtoFile) || die "Could not copy updated GTO: $!";
+}
 
